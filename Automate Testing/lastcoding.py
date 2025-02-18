cfcb37790eb0afe79daaa9ee7a3bd3809026c589
callmeohmnaja <airathiwat140745@gmail.com>
2025-02-18 16:47:34 +0700

    done มั้ง

--- Automate Testing/lastcoding.py
+++ Automate Testing/lastcoding.py
@@ -21,8 +21,6 @@ Login.click()
 driver.get('http://172.26.3.11:8080/jw/web/userview/HRM_Recruitment/v/_/Welcome')
 driver.get('http://172.26.3.11:8080/jw/web/userview/HRM_Recruitment/v/_/5B4BCB9413D64BF0ACABA4F66F302079?_action=assignmentView&activityId=7233599_50470_HRM_Recruitment_process64_activity1')
 
-add = driver.find_element(By.CLASS_NAME, 'grid-action-add')
-add.click()
 
 input("ป้อนค่าเพื่อจบการทํางาน")
 driver.quit()
